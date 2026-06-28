14094ef0d37fca6fed89eaa1a57dcc6fb8e81bf9
dwege <ojorodavid@gmail.com>
2026-06-28 20:32:21 +0300

    debugging the frontend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
+const BASE_URL = 'https://danis-choice-website-3.onrender.com/api';
 console.log("CURRENT API URL:", BASE_URL);
 const api = axios.create({ baseURL: BASE_URL });
 
